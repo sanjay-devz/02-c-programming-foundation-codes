7b8272326dee95f053f58d547bd904e8a0da1ada
sanjay-devz <sanjay.gridz@gmail.com>
2026-08-30 11:59:29 +0530

    file name changed

---
+++ AgeStringFormater.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = 34;
+    printf("He is %d age catagory person\n", a);
+
+    float cgpa = 2.4;
+    float percentage = 75.23;
+    printf("I got %.1f in my collage UG degree\n", cgpa);
+    printf("I got %.2f in my 10th standard\n", percentage);
+}
--- IntergerLearning.c
+++ IntergerLearning.c
@@ -3,7 +3,10 @@
 
 int main(void)
 {
+    // the program to print something 
     printf("Hello world, it's working now!\n");
+    int a = 10;
+    printf("embeding files!!\n");
     printf("Works properly.\n");
 
     return 0;
