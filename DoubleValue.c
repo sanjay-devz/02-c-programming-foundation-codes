6baf763ef60400cbf386d626cc557b0abe120371
sanjay-devz <sanjay.gridz@gmail.com>
2026-08-30 17:06:39 +0530

    The data types are currently learning

---
+++ DoubleValue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    double pi = 3.124343543654654654;
+    double e = 2.3343534645765756;
+
+    printf("The value of pi is %.15lf\n", pi);
+    printf("The value of e is %.15lf\n", e);
+    printf("Hello world\n");
+    char a = '+A';
+    printf("grade is %c \n ", a);
+}
