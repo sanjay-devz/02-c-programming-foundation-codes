d0b460a35ef8f5c9e38627878587f1c1c794bb50
sanjay-devz <sanjay.gridz@gmail.com>
2026-09-03 06:19:47 +0530

    C programming conditional statement is added to repo

---
+++ ConditionalStatement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+    int firstNum = 20;
+    int secondNum = 10;
+
+    if (firstNum > secondNum){
+        printf("Its Greater Than that!!");
+    } else{
+        printf("Its lower!!");
+    }
+
+
+}
