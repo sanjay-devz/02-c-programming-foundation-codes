ccb8b29958d76aa4823e729f83097e9f05330e0f
sanjay-devz <sanjay.gridz@gmail.com>
2026-09-06 16:28:16 +0530

    scanf user input int and float works char pending

--- Foundational-codes/scanf-userInput.c
+++ Foundational-codes/scanf-userInput.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world!");
+    int age = 0;
+    float percentage = 0.0;
+    char grade = '\0';
+
+    printf("Enter Your Age:");
+    scanf("%d", &age);
+
+    printf("Enter Your Percentage:");
+    scanf("%f", &percentage);
+
+    printf("Enter Your Grade:");
+    scanf("%c", &grade);
+
+
     return 0;
 }
